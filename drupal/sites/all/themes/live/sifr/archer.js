8f60f66ffa73cbc91761838a0792d5c08f11013e
KatyMoon <katyjockelson@thirdsectordesign.org>
2011-11-22 16:24:17 +0000

    graphics for manin nav

--- drupal/sites/all/themes/live/sifr/archer.js
+++ drupal/sites/all/themes/live/sifr/archer.js
@@ -3,4 +3,3 @@ sIFR.replaceElement("h3, h2, .views-field-title span", "/sites/all/themes/live/s
 sIFR.replaceElement(".hp-quote, .cameron", "/sites/all/themes/live/sifr/fonts/archer-bolditalic.swf", named({sColor : "#24C4F4" , sWmode:"transparent" , sFlashVars: "textalign=right"}));
 sIFR.replaceElement(".intro", "/sites/all/themes/live/sifr/fonts/archer-medium.swf", named({sColor : "#232323" , sWmode:"transparent" , sFlashVars: "textalign=right"}));
 sIFR.replaceElement(".signup-text", "/sites/all/themes/live/sifr/fonts/archer-medium.swf", named({sColor : "#ffffff" , sWmode:"transparent"}));
-sIFR.replaceElement("#navigation a", "/sites/all/themes/live/sifr/fonts/archer-medium.swf", named({sColor : "#232323" , sWmode:"transparent" , sFlashVars: "textalign=center"}));
