834f983d83efed76ffeea6ea0dd92a7935b95e0a
KatyMoon <katyjockelson@thirdsectordesign.org>
2011-11-22 10:24:22 +0000

    hr style

--- drupal/sites/all/themes/live/sifr/archer.js
+++ drupal/sites/all/themes/live/sifr/archer.js
@@ -1,4 +1,4 @@
-sIFR.replaceElement("h1, .quoted, quote", "/sites/all/themes/live/sifr/fonts/archer-bolditalic.swf", named({sColor : "#232323" , sWmode:"transparent"}));
+sIFR.replaceElement("h1, .quoted", "/sites/all/themes/live/sifr/fonts/archer-bolditalic.swf", named({sColor : "#232323" , sWmode:"transparent"}));
 sIFR.replaceElement("h3, h2, .views-field-title span", "/sites/all/themes/live/sifr/fonts/archer-bolditalic.swf", named({sColor : "#24C4F4" , sWmode:"transparent"}));
 sIFR.replaceElement(".hp-quote, .cameron", "/sites/all/themes/live/sifr/fonts/archer-bolditalic.swf", named({sColor : "#24C4F4" , sWmode:"transparent" , sFlashVars: "textalign=right"}));
 sIFR.replaceElement(".intro", "/sites/all/themes/live/sifr/fonts/archer-medium.swf", named({sColor : "#232323" , sWmode:"transparent" , sFlashVars: "textalign=right"}));
