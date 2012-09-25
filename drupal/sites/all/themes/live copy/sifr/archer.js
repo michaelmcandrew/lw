e4005e8062c75eabb9e1f3bc2b123202ef5b5002
KatyMoon <katyjockelson@thirdsectordesign.org>
2012-09-25 11:56:00 +0100

    changes to hp layout, block class module and form styles

---
+++ drupal/sites/all/themes/live copy/sifr/archer.js
@@ -0,0 +1,6 @@
+sIFR.replaceElement("h1, .quoted", "/sites/all/themes/live/sifr/fonts/archer-bolditalic.swf", named({sColor : "#232323" , sWmode:"transparent"}));
+sIFR.replaceElement("h3, h2, .views-field-title span", "/sites/all/themes/live/sifr/fonts/archer-bolditalic.swf", named({sColor : "#24C4F4" , sWmode:"transparent"}));
+sIFR.replaceElement(".cameron", "/sites/all/themes/live/sifr/fonts/archer-bolditalic.swf", named({sColor : "#24C4F4" , sWmode:"transparent", sFlashVars: "textalign=right"}));
+sIFR.replaceElement("#hp-text", "/sites/all/themes/live/sifr/fonts/archer-medium.swf", named({sColor : "#24C4F4" , sWmode:"transparent"}));
+sIFR.replaceElement(".intro", "/sites/all/themes/live/sifr/fonts/archer-medium.swf", named({sColor : "#232323" , sWmode:"transparent" , sFlashVars: "textalign=right"}));
+sIFR.replaceElement(".signup-text", "/sites/all/themes/live/sifr/fonts/archer-medium.swf", named({sColor : "#ffffff" , sWmode:"transparent"}));
--- drupal/sites/all/themes/live/sifr/archer.js
+++ drupal/sites/all/themes/live/sifr/archer.js
@@ -1,5 +1,11 @@
-sIFR.replaceElement("h1, .quoted", "/sites/all/themes/live/sifr/fonts/archer-bolditalic.swf", named({sColor : "#232323" , sWmode:"transparent"}));
-sIFR.replaceElement("h3, h2, .views-field-title span", "/sites/all/themes/live/sifr/fonts/archer-bolditalic.swf", named({sColor : "#24C4F4" , sWmode:"transparent"}));
-sIFR.replaceElement(".hp-quote, .cameron", "/sites/all/themes/live/sifr/fonts/archer-bolditalic.swf", named({sColor : "#24C4F4" , sWmode:"transparent" , sFlashVars: "textalign=right"}));
-sIFR.replaceElement(".intro", "/sites/all/themes/live/sifr/fonts/archer-medium.swf", named({sColor : "#232323" , sWmode:"transparent" , sFlashVars: "textalign=right"}));
-sIFR.replaceElement(".signup-text", "/sites/all/themes/live/sifr/fonts/archer-medium.swf", named({sColor : "#ffffff" , sWmode:"transparent"}));
+sIFR.replaceElement("h1, .quoted", "/sites/all/themes/live/sifr/fonts/archer-bolditalic.swf", named({sColor : "#232323" , sWmode: "transparent"}));
+sIFR.replaceElement("h3, h2, .views-field-title span", "/sites/all/themes/live/sifr/fonts/archer-bolditalic.swf", named({sColor : "#24C4F4" , sWmode: "transparent"}));
+
+/* quote */
+sIFR.replaceElement(".cameron .dc, .hp-employer-quote .views-field-field-quote", "/sites/all/themes/live/sifr/fonts/archer-bolditalic.swf", named({sColor : "#232323" , sWmode: "transparent"}));
+sIFR.replaceElement(".cameron .dc-credit, .hp-employer-quote .views-field-field-author", "/sites/all/themes/live/sifr/fonts/archer-mediumitalic.swf", named({sColor : "#232323" , sWmode: "transparent", sFlashVars: "textalign=right"}));
+
+/* hp text */
+sIFR.replaceElement("#hp-text", "/sites/all/themes/live/sifr/fonts/archer-mediumitalic.swf", named({sColor : "#24C4F4" , sWmode: "transparent"}));
+
+sIFR.replaceElement(".signup-text", "/sites/all/themes/live/sifr/fonts/archer-medium.swf", named({sColor : "#ffffff" , sWmode: "transparent"}));
