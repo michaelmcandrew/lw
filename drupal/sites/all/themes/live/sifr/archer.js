d421fd81335dae37c7d2fa61fa6d8baf83aaf9ae
KatyMoon <katyjockelson@thirdsectordesign.org>
2012-10-02 16:56:16 +0100

    adding line-height on h2

--- drupal/sites/all/themes/live/sifr/archer.js
+++ drupal/sites/all/themes/live/sifr/archer.js
@@ -2,8 +2,8 @@ sIFR.replaceElement("h1, .quoted", "/sites/all/themes/live/sifr/fonts/archer-bol
 sIFR.replaceElement("h3, h2, .views-field-title span", "/sites/all/themes/live/sifr/fonts/archer-bolditalic.swf", named({sColor : "#24C4F4" , sWmode: "transparent"}));
 
 /* quote */
-sIFR.replaceElement(".cameron .dc, .hp-employer-quote .views-field-field-quote", "/sites/all/themes/live/sifr/fonts/archer-bolditalic.swf", named({sColor : "#232323" , sWmode: "transparent"}));
-sIFR.replaceElement(".cameron .dc-credit, .hp-employer-quote .views-field-field-author", "/sites/all/themes/live/sifr/fonts/archer-mediumitalic.swf", named({sColor : "#232323" , sWmode: "transparent", sFlashVars: "textalign=right"}));
+sIFR.replaceElement(".sidebar-quote .quote, .hp-employer-quote .views-field-field-quote", "/sites/all/themes/live/sifr/fonts/archer-bolditalic.swf", named({sColor : "#232323" , sWmode: "transparent"}));
+sIFR.replaceElement(".sidebar-quote .credit, .hp-employer-quote .views-field-field-author", "/sites/all/themes/live/sifr/fonts/archer-mediumitalic.swf", named({sColor : "#232323" , sWmode: "transparent", sFlashVars: "textalign=right"}));
 
 /* hp text */
 sIFR.replaceElement("#hp-text", "/sites/all/themes/live/sifr/fonts/archer-bolditalic.swf", named({sColor : "#24C4F4" , sWmode: "transparent"}));
